57aaee06d767d0287b90e25bf0099c44737d65a8
Justin Fiset <justinfiset@hotmail.fr>
2025-03-11 16:47:51 -0400

    Création du menu principal UI et désign

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("clicked");
+        Application.Quit();
+    }
+}
