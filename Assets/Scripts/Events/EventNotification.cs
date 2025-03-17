20fbc03ca2b609cd48d06835cfca8a5a1cffb68b
Justin Fiset <justinfiset@hotmail.fr>
2025-03-17 09:07:36 -0400

    Ajout d'animation aux notifications

--- Assets/Scripts/Events/EventNotification.cs
+++ Assets/Scripts/Events/EventNotification.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class EventNotification : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI nameText;
     [SerializeField] private TextMeshProUGUI durationText;
 
+    private CanvasGroup canvasGroup;
+
+    public void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0.0f;
+        LeanTween.alphaCanvas(canvasGroup, 1.0f, 0.2f);
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            EndEvent();
+        }
+    }
+
+    public void EndEvent()
+    {
+        LeanTween.alphaCanvas(canvasGroup, 0.0f, 0.3f).setOnComplete(()=>
+        {
+            // TODO: NOTIFIER LE NOTIFICATION CONTROLLER
+            Destroy(gameObject);
+        });
+    }
+
     public void Init(string name, int duration)
     {
         nameText.SetText(name);
-        nameText.SetText(duration + "s");
+        durationText.SetText(duration + "s");
     }
 }
