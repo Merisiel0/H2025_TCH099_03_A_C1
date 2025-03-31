64684c9e3393c20a361dd22033e54d878a655871
Merisiel0 <merisiel01@gmail.com>
2025-03-31 15:03:08 -0400

    implemented levitating lights module

---
+++ Assets/Scripts/Module/LevitatingLights/LevitatingLightsModule.cs
@@ -0,0 +1,116 @@
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Collections;
+using Unity.Mathematics;
+
+public class LevitatingLightsModule : ModuleUI, MissionEventListener
+{
+    public Color lightOn;
+    public Color lightOff;
+    public Color leverColor;
+    [SerializeField] private GameObject lightsContainer;
+    [SerializeField] private GameObject leversContainer;
+    private Image[] lights;
+    private Image[] levers;
+    private bool[] solution;
+    private LevitatingLightsModuleData data;
+    private bool activated = false;
+
+    public void OnNotify(MissionEvent e)
+    {
+        if (e == MissionEvent.LightsOut)
+        {
+            data = new LevitatingLightsModuleData();
+            data.lights = "XOOXOXOXO";
+            data.solution = "100101";
+
+            InitModule(data);
+        }
+    }
+
+    private void Start()
+    {
+        base.Start();
+
+        data = new LevitatingLightsModuleData();
+
+        lights = lightsContainer.GetComponentsInChildren<Image>();
+        levers = leversContainer.GetComponentsInChildren<Image>();
+
+        foreach (Image light in lights)
+        {
+            light.color = lightOff;
+        }
+
+        foreach (Image lever in levers)
+        {
+            lever.color = leverColor;
+        }
+
+        MissionEventManager.AddEventListener(this);
+    }
+
+    public void InitModule(LevitatingLightsModuleData newData)
+    {
+        data = newData.Init();
+
+        solution = new bool[data.solution.Length];
+        for (int i = 0; i < solution.Length; i++)
+        {
+            solution[i] = data.solution[i] == '1';
+        }
+
+        for (int i = 0; i < lights.Length; i++)
+        {
+            if (data.lights[i] == 'O')
+            {
+                lights[i].color = lightOn;
+            }
+        }
+
+        activated = true;
+    }
+
+    public void OnLeverSwitched(int index)
+    {
+        // toggle lever
+        ToggleLever(index);
+
+        if (activated)
+        {
+            // check if all levers are good
+            for (int i = 0; i < levers.Length; i++)
+            {
+                if (solution[i] != IsLeverToggled(i))
+                {
+                    return;
+                }
+            }
+
+            // if all levers are good, quit module
+            PlayerInteract.StopInteractions();
+            activated = false;
+        }
+    }
+
+
+    private bool IsLeverToggled(int i)
+    {
+        return levers[i].rectTransform.localScale.y < 0;
+    }
+
+    private void ToggleLever(int i)
+    {
+        Vector3 scale = levers[i].rectTransform.localScale;
+        scale.y *= -1;
+        levers[i].rectTransform.localScale = scale;
+
+        int n = IsLeverToggled(i) ? -1 : 1;
+        levers[i].rectTransform.anchoredPosition += n * new Vector2(1, 29);
+    }
+}
