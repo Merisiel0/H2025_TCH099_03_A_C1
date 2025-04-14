d7f4210264cbfb2aea8d33c7126f6db0dd531c54
Justin Fiset <justinfiset@hotmail.fr>
2025-04-14 19:23:51 -0400

    User Signup

---
+++ Assets/Scripts/UI/SignupMenu.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+[System.Serializable]
+public class SignupResponse
+{
+    public string message;
+}
+
+[System.Serializable]
+public class SignupError
+{
+    public string error;
+}
+
+public class SignupRequest
+{
+    public string pseudo;
+    public string mdp;
+}
+
+public class SignupMenu : MonoBehaviour
+{
+    [SerializeField] MainMenu mainMenu;
+    private static string apiRoute = "/api/v1/utilisateur";
+
+    [SerializeField] private TMP_InputField usernameInupt;
+    [SerializeField] private TMP_InputField passwordInput;
+    [SerializeField] private TMP_InputField passwordConfirmInput;
+    [SerializeField] private TextMeshProUGUI errorText;
+
+    public void Signup()
+    {
+        // Vérification si tous les champs ont été remplit
+        if (usernameInupt.text.Length < 1 || passwordInput.text.Length < 1 || passwordConfirmInput.text.Length < 1)
+        {
+            errorText.gameObject.SetActive(true);
+            errorText.text = "Veuillez remplir tous les champs.";
+            return;
+        }
+
+        // Vérification si les deux mdp sont pareil
+        if (passwordInput.text != passwordConfirmInput.text)
+        {
+            errorText.gameObject.SetActive(true);
+            errorText.text = "Les mots de passe ne correspondent pas. Veuillez réessayer.";
+            return;
+        }
+
+        // Si les vérification passent, on envoie la requête
+        SignupRequest request = new SignupRequest();
+        request.pseudo = usernameInupt.text;
+        request.mdp = passwordInput.text;
+
+        string body = JsonUtility.ToJson(request);
+        ApiController.PostJsonToAPI(apiRoute, body, (data) =>
+        {
+            try
+            {
+                SignupResponse rep = JsonUtility.FromJson<SignupResponse>(data);
+
+                // Si on a bien recu une réponse
+                if (rep != null)
+                {
+                    errorText.gameObject.SetActive(false);
+                    Debug.Log("Utilisateur Créer!");
+                    mainMenu.OpenConnexionMenu();
+
+                    // Reset des champs
+                    usernameInupt.text = "";
+                    passwordInput.text = "";
+                    passwordConfirmInput.text = "";
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch (Exception e)
+            {
+                UserConnectionObject.Disconnect();
+                ConnectionError errorObj = JsonUtility.FromJson<ConnectionError>(data);
+
+                errorText.gameObject.SetActive(true);
+                errorText.text = (errorObj != null) ? errorObj.error : "Erreur de connection.";
+            }
+        });
+    }
+}
